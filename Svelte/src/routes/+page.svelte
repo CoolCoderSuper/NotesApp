858fb4a60a44c715e442ef516fce3a14e11074bc
Joseph Guenther <joseph@codingcool.ca>
2022-12-19 20:10:23 -0500

    Add project files.

---
+++ Svelte/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	let count = 0
+	let click_me = () => {
+		count += 1
+	}
+	let or_me = () => {
+		count -= 1
+	}
+</script>
+
+<section>
+	<div>The number is: {count}</div>
+	<button class="btn btn-primary" on:click={click_me}>Click me</button>
+	<button class="btn btn-secondary" on:click={or_me}>Or me</button>
+</section>
