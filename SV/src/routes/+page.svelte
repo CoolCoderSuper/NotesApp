f3fff3615ffb90fbb982c8dacb0f9a3655dc2089
Joseph Guenther <76707331+CoolCoderSuper@users.noreply.github.com>
2023-04-05 21:59:46 +0000

    Add svelte app

---
+++ SV/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<h1>Notes</h1>
+
+<script lang="ts">
+    let notes = [
+        {content: 'Note 1'},
+        {content: 'Note 2'},
+        {content: 'Note 3'},
+    ];
+</script>
+
+<div style="padding-left: 5px;">
+    <button class="btn btn-primary">Add</button>
+</div>
+{#each notes as note}
+    <div style="padding: 5px;">
+        <textarea class="form-control" placeholder="Note" bind:value="{note.content}"/>
+        <button class="btn btn-danger">Delete</button>
+    </div>
+{/each}
