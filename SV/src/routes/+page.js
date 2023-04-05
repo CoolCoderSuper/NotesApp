ef5960b757eab50efbcb3e0042b7f5e86bfc5314
Joseph Guenther <76707331+CoolCoderSuper@users.noreply.github.com>
2023-04-05 23:07:34 +0000

    Finish svelte project

--- SV/src/routes/+page.js
+++ SV/src/routes/+page.js
@@ -3,5 +3,5 @@ export async function load({ fetch, params }) {
     const res = await fetch(`https://coolcodersuper-crispy-potato-9g5q6w4pj52xx4j-5124.preview.app.github.dev/Notes`);
     const item = await res.json();
    
-    return item;
-  }
+    return { notes: item };
+}
--- SV/src/routes/+page.svelte
+++ SV/src/routes/+page.svelte
@@ -3,15 +3,49 @@
 <script>
   /** @type {import('./$types').LayoutData} */
   export let data;
-  console.log(data);
+
+    async function load()
+    {
+        const res = await fetch(`https://coolcodersuper-crispy-potato-9g5q6w4pj52xx4j-5124.preview.app.github.dev/Notes`);
+        data.notes = await res.json();
+    }
 </script>
 
 <div style="padding-left: 5px;">
-    <button class="btn btn-primary">Add</button>
+    <button class="btn btn-primary" on:click={async () =>
+        {
+            let note = {
+                    id: 0,
+                    content: ''
+                }
+            const res = await fetch(`https://coolcodersuper-crispy-potato-9g5q6w4pj52xx4j-5124.preview.app.github.dev/Notes`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(note)
+            });
+            await load();
+        }}>Add</button>
 </div>
-{#each data as note}
+{#each data.notes as note}
     <div style="padding: 5px;">
-        <textarea class="form-control" placeholder="Note" bind:value="{note.content}"/>
-        <button class="btn btn-danger">Delete</button>
+        <textarea class="form-control" placeholder="Note" bind:value={note.content} on:input={async () => {
+            note.content = event.target.value;
+            const res = await fetch(`https://coolcodersuper-crispy-potato-9g5q6w4pj52xx4j-5124.preview.app.github.dev/Notes/`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(note)
+            });
+            await load();
+        }}/>
+        <button class="btn btn-danger" on:click={async () => {
+            const res = await fetch(`https://coolcodersuper-crispy-potato-9g5q6w4pj52xx4j-5124.preview.app.github.dev/Notes/${note.id}`, {
+                method: 'DELETE'
+            });
+            await load();
+        }}>Delete</button>
     </div>
 {/each}
