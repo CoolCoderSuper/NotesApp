012cf03685d3990754de37a93a4d73a1fa2576d0
Joseph Guenther <76707331+CoolCoderSuper@users.noreply.github.com>
2023-04-05 22:22:29 +0000

    Changes

---
+++ SV/src/routes/+page.js
@@ -0,0 +1,7 @@
+/** @type {import('./$types').PageLoad} */
+export async function load({ fetch, params }) {
+    const res = await fetch(`https://coolcodersuper-crispy-potato-9g5q6w4pj52xx4j-5124.preview.app.github.dev/Notes`);
+    const item = await res.json();
+   
+    return item;
+  }
--- SV/src/routes/+page.svelte
+++ SV/src/routes/+page.svelte
@@ -1,18 +1,15 @@
 <h1>Notes</h1>
 
 <script>
-
-    let notes = [
-        {content: 'Note 1'},
-        {content: 'Note 2'},
-        {content: 'Note 3'},
-    ];
+  /** @type {import('./$types').LayoutData} */
+  export let data;
+  console.log(data);
 </script>
 
 <div style="padding-left: 5px;">
     <button class="btn btn-primary">Add</button>
 </div>
-{#each notes as note}
+{#each data as note}
     <div style="padding: 5px;">
         <textarea class="form-control" placeholder="Note" bind:value="{note.content}"/>
         <button class="btn btn-danger">Delete</button>
