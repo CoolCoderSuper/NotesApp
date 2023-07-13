f3426658d006705a053c6fb7f60272eb616c0d17
Joseph Guenther <joseph@codingcool.ca>
2023-07-13 11:12:52 -0400

    Add convert browser to vb

---
+++ notes/notes.Browser/Program.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Runtime.Versioning
+Imports System.Threading.Tasks
+Imports Avalonia
+Imports Avalonia.Browser
+Imports Avalonia.ReactiveUI
+Imports notes
+
+<Assembly: SupportedOSPlatform("browser")>
+
+Friend Module Program
+    Public Sub Main(args As String())
+        BuildAvaloniaApp().WithInterFont().UseReactiveUI().StartBrowserAppAsync("out")
+    End Sub
+
+    Public Function BuildAvaloniaApp() As AppBuilder
+        Return AppBuilder.Configure(Of App)()
+    End Function
+End Module
