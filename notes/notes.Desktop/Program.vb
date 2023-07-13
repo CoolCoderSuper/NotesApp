15967069725ba1daf64662ea2e81023641393cd1
Joseph Guenther <joseph@codingcool.ca>
2023-07-13 11:00:58 -0400

    Convert desktop to vb

---
+++ notes/notes.Desktop/Program.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports Avalonia
+Imports Avalonia.ReactiveUI
+
+Friend Class Program
+    ' Initialization code. Don't use any Avalonia, third-party APIs or any
+    ' SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    ' yet and stuff might break.
+    <STAThread>
+    Public Shared Sub Main(ByVal args As String())
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args)
+    End Sub
+
+    ' Avalonia configuration, don't remove; also used by visual designer.
+    Public Shared Function BuildAvaloniaApp() As AppBuilder
+        Return AppBuilder.Configure(Of App)().UsePlatformDetect().WithInterFont().LogToTrace().UseReactiveUI()
+    End Function
+End Class
